67e386a96700130105ea6c137614e31fbf77ba9c
laurens <miers132@gmail.com>
2019-11-15 13:34:25 +0100

    Actually use the inline mocks to make the test pass
    - If a mock is not produced, we should get an error

--- test/test_example.c
+++ test/test_example.c
@@ -13,70 +13,102 @@ void tearDown(void)
 
 void test_example(void)
 {
-	alwaysinline_func_0(1);
+	alwaysinline_func_0_ExpectAndReturn(1, 1);
+	TEST_ASSERT_EQUAL(1, alwaysinline_func_0(1));
 
-	alwaysinline_func_1(1);
+	alwaysinline_func_1_ExpectAndReturn(1,1);
+	TEST_ASSERT_EQUAL(1, alwaysinline_func_1(1));
 
-	alwaysinline_func_2(1);
+	alwaysinline_func_2_ExpectAndReturn(1,1);
+	TEST_ASSERT_EQUAL(1, alwaysinline_func_2(1));
 
-	alwaysinline_func_3(1);
+	alwaysinline_func_3_ExpectAndReturn(1,1);
+	TEST_ASSERT_EQUAL(1, alwaysinline_func_3(1));
 
-	alwaysinline_func_4(1);
+	alwaysinline_func_4_ExpectAndReturn(1,1);
+	TEST_ASSERT_EQUAL(1, alwaysinline_func_4(1));
 
-	alwaysinline_func_5(1);
+	alwaysinline_func_5_ExpectAndReturn(1,1);
+	TEST_ASSERT_EQUAL(1, alwaysinline_func_5(1));
 
-	alwaysinline_func_6(1);
+	alwaysinline_func_6_ExpectAndReturn(1,1);
+	TEST_ASSERT_EQUAL(1, alwaysinline_func_6(1));
 
-	alwaysinline_func_7(1);
+	alwaysinline_func_7_ExpectAndReturn(1,1);
+	TEST_ASSERT_EQUAL(1, alwaysinline_func_7(1));
 
-	alwaysinline_func_8(1);
+	alwaysinline_func_8_ExpectAndReturn(1,1);
+	TEST_ASSERT_EQUAL(1, alwaysinline_func_8(1));
 
-	alwaysinline_func_9(1);
+	alwaysinline_func_9_ExpectAndReturn(1,1);
+	TEST_ASSERT_EQUAL(1, alwaysinline_func_9(1));
 
-	alwaysinline_func_10(1);
+	alwaysinline_func_10_ExpectAndReturn(1,1);
+	TEST_ASSERT_EQUAL(1, alwaysinline_func_10(1));
 
-	alwaysinline_func_11(1);
+	alwaysinline_func_11_ExpectAndReturn(1,1);
+	TEST_ASSERT_EQUAL(1, alwaysinline_func_11(1));
 
-	alwaysinline_func_12(1);
+	alwaysinline_func_12_ExpectAndReturn(1,1);
+	TEST_ASSERT_EQUAL(1, alwaysinline_func_12(1));
 
-	alwaysinline_func_13(1);
+	alwaysinline_func_13_ExpectAndReturn(1,1);
+	TEST_ASSERT_EQUAL(1, alwaysinline_func_13(1));
 
-	alwaysinline_func_14(1);
+	alwaysinline_func_14_ExpectAndReturn(1,1);
+	TEST_ASSERT_EQUAL(1, alwaysinline_func_14(1));
 
-	alwaysinline_func_15(1);
+	alwaysinline_func_15_ExpectAndReturn(1,1);
+	TEST_ASSERT_EQUAL(1, alwaysinline_func_15(1));
 
-	inline_func_0(1);
+	inline_func_0_ExpectAndReturn(1,1);
+	TEST_ASSERT_EQUAL(1, inline_func_0(1));
 
-	inline_func_1(1);
+	inline_func_1_ExpectAndReturn(1,1);
+	TEST_ASSERT_EQUAL(1, inline_func_1(1));
 
-	inline_func_2(1);
+	inline_func_2_ExpectAndReturn(1,1);
+	TEST_ASSERT_EQUAL(1, inline_func_2(1));
 
-	inline_func_3(1);
+	inline_func_3_ExpectAndReturn(1,1);
+	TEST_ASSERT_EQUAL(1, inline_func_3(1));
 
-	inline_func_4(1);
+	inline_func_4_ExpectAndReturn(1,1);
+	TEST_ASSERT_EQUAL(1, inline_func_4(1));
 
-	inline_func_5(1);
+	inline_func_5_ExpectAndReturn(1,1);
+	TEST_ASSERT_EQUAL(1, inline_func_5(1));
 
-	inline_func_6(1);
+	inline_func_6_ExpectAndReturn(1,1);
+	TEST_ASSERT_EQUAL(1, inline_func_6(1));
 
-	inline_func_7(1);
+	inline_func_7_ExpectAndReturn(1,1);
+	TEST_ASSERT_EQUAL(1, inline_func_7(1));
 
-	inline_func_8(1);
+	inline_func_8_ExpectAndReturn(1,1);
+	TEST_ASSERT_EQUAL(1, inline_func_8(1));
 
-	inline_func_9(1);
+	inline_func_9_ExpectAndReturn(1,1);
+	TEST_ASSERT_EQUAL(1, inline_func_9(1));
 
-	inline_func_10(1);
+	inline_func_10_ExpectAndReturn(1,1);
+	TEST_ASSERT_EQUAL(1, inline_func_10(1));
 
-	inline_func_11(1);
+	inline_func_11_ExpectAndReturn(1,1);
+	TEST_ASSERT_EQUAL(1, inline_func_11(1));
 
-	inline_func_12(1);
+	inline_func_12_ExpectAndReturn(1,1);
+	TEST_ASSERT_EQUAL(1, inline_func_12(1));
 
-	inline_func_13(1);
+	inline_func_13_ExpectAndReturn(1,1);
+	TEST_ASSERT_EQUAL(1, inline_func_13(1));
 
-	inline_func_14(1);
+	inline_func_14_ExpectAndReturn(1,1);
+	TEST_ASSERT_EQUAL(1, inline_func_14(1));
 
-	inline_func_15(1);
+	inline_func_15_ExpectAndReturn(1,1);
+	TEST_ASSERT_EQUAL(1, inline_func_15(1));
 
-	inline_func_16(1);
-
+	inline_func_16_ExpectAndReturn(1,1);
+	TEST_ASSERT_EQUAL(1, inline_func_16(1));
 }
