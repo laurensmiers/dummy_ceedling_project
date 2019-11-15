fc2cc93114f2a4b4be123fd100825a4755a4eb1f
laurens <miers132@gmail.com>
2019-11-15 13:11:07 +0100

    Initial commit

---
+++ src/example.h
@@ -0,0 +1,169 @@
+#ifndef _EXAMPLE_H
+#define _EXAMPLE_H
+
+static __inline__ __attribute__ ((always_inline)) uint16_t alwaysinline_func_0 (uint32_t a)
+{
+	return (uint16_t)a;
+}
+
+static __inline__ __attribute__ ((always_inline)) uint16_t alwaysinline_func_1 (uint32_t a)
+{
+	return (uint16_t)a;
+}
+
+static __inline__ __attribute__ ((always_inline)) uint16_t alwaysinline_func_2 (uint32_t a)
+{
+	return (uint16_t)a;
+}
+
+static __inline__ __attribute__ ((always_inline)) uint16_t alwaysinline_func_3 (uint32_t a)
+{
+	return (uint16_t)a;
+}
+
+static __inline__ __attribute__ ((always_inline)) uint16_t alwaysinline_func_4 (uint32_t a)
+{
+	return (uint16_t)a;
+}
+
+static __inline__ __attribute__ ((always_inline)) uint16_t alwaysinline_func_5 (uint32_t a)
+{
+	return (uint16_t)a;
+}
+
+static __inline__ __attribute__ ((always_inline)) uint16_t alwaysinline_func_6 (uint32_t a)
+{
+	return (uint16_t)a;
+}
+
+static __inline__ __attribute__ ((always_inline)) uint16_t alwaysinline_func_7 (uint32_t a)
+{
+	return (uint16_t)a;
+}
+
+static __inline__ __attribute__ ((always_inline)) uint16_t alwaysinline_func_8 (uint32_t a)
+{
+	return (uint16_t)a;
+}
+
+static __inline__ __attribute__ ((always_inline)) uint16_t alwaysinline_func_9 (uint32_t a)
+{
+	return (uint16_t)a;
+}
+
+static __inline__ __attribute__ ((always_inline)) uint16_t alwaysinline_func_10 (uint32_t a)
+{
+	return (uint16_t)a;
+}
+
+static __inline__ __attribute__ ((always_inline)) uint16_t alwaysinline_func_11 (uint32_t a)
+{
+	return (uint16_t)a;
+}
+
+static __inline__ __attribute__ ((always_inline)) uint16_t alwaysinline_func_12 (uint32_t a)
+{
+	return (uint16_t)a;
+}
+
+static __inline__ __attribute__ ((always_inline)) uint16_t alwaysinline_func_13 (uint32_t a)
+{
+	return (uint16_t)a;
+}
+
+static __inline__ __attribute__ ((always_inline)) uint16_t alwaysinline_func_14 (uint32_t a)
+{
+	return (uint16_t)a;
+}
+
+static __inline__ __attribute__ ((always_inline)) uint16_t alwaysinline_func_15 (uint32_t a)
+{
+	return (uint16_t)a;
+}
+
+static __inline__ uint16_t inline_func_0  (uint32_t a)
+{
+    return (uint16_t) a;
+}
+
+static __inline__ uint16_t inline_func_1  (uint32_t a)
+{
+    return (uint16_t) a;
+}
+
+static __inline__ uint16_t inline_func_2  (uint32_t a)
+{
+    return (uint16_t) a;
+}
+
+static __inline__ uint16_t inline_func_3  (uint32_t a)
+{
+    return (uint16_t) a;
+}
+
+static __inline__ uint16_t inline_func_4  (uint32_t a)
+{
+    return (uint16_t) a;
+}
+
+static __inline__ uint16_t inline_func_5  (uint32_t a)
+{
+    return (uint16_t) a;
+}
+
+static __inline__ uint16_t inline_func_6  (uint32_t a)
+{
+    return (uint16_t) a;
+}
+
+static __inline__ uint16_t inline_func_7  (uint32_t a)
+{
+    return (uint16_t) a;
+}
+
+static __inline__ uint16_t inline_func_8  (uint32_t a)
+{
+    return (uint16_t) a;
+}
+
+static __inline__ uint16_t inline_func_9  (uint32_t a)
+{
+    return (uint16_t) a;
+}
+
+static __inline__ uint16_t inline_func_10  (uint32_t a)
+{
+    return (uint16_t) a;
+}
+
+static __inline__ uint16_t inline_func_11  (uint32_t a)
+{
+    return (uint16_t) a;
+}
+
+static __inline__ uint16_t inline_func_12  (uint32_t a)
+{
+    return (uint16_t) a;
+}
+
+static __inline__ uint16_t inline_func_13  (uint32_t a)
+{
+    return (uint16_t) a;
+}
+
+static __inline__ uint16_t inline_func_14  (uint32_t a)
+{
+    return (uint16_t) a;
+}
+
+static __inline__ uint16_t inline_func_15  (uint32_t a)
+{
+    return (uint16_t) a;
+}
+
+static __inline__ uint16_t inline_func_16  (uint32_t a)
+{
+    return (uint16_t) a;
+}
+
+#endif /* _EXAMPLE_H */
---
+++ test/test_example.c
@@ -0,0 +1,82 @@
+#include "unity.h"
+
+#include "mock_example.h"
+
+
+void setUp(void)
+{
+}
+
+void tearDown(void)
+{
+}
+
+void test_example(void)
+{
+	alwaysinline_func_0(1);
+
+	alwaysinline_func_1(1);
+
+	alwaysinline_func_2(1);
+
+	alwaysinline_func_3(1);
+
+	alwaysinline_func_4(1);
+
+	alwaysinline_func_5(1);
+
+	alwaysinline_func_6(1);
+
+	alwaysinline_func_7(1);
+
+	alwaysinline_func_8(1);
+
+	alwaysinline_func_9(1);
+
+	alwaysinline_func_10(1);
+
+	alwaysinline_func_11(1);
+
+	alwaysinline_func_12(1);
+
+	alwaysinline_func_13(1);
+
+	alwaysinline_func_14(1);
+
+	alwaysinline_func_15(1);
+
+	inline_func_0(1);
+
+	inline_func_1(1);
+
+	inline_func_2(1);
+
+	inline_func_3(1);
+
+	inline_func_4(1);
+
+	inline_func_5(1);
+
+	inline_func_6(1);
+
+	inline_func_7(1);
+
+	inline_func_8(1);
+
+	inline_func_9(1);
+
+	inline_func_10(1);
+
+	inline_func_11(1);
+
+	inline_func_12(1);
+
+	inline_func_13(1);
+
+	inline_func_14(1);
+
+	inline_func_15(1);
+
+	inline_func_16(1);
+
+}
